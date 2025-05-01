4de93870fdbf6f4e82ffdc246c04fc411692c525
GRIGS <lalov@ssps.dev>
2025-05-01 19:23:20 +0300

    add: private route component

--- App/front-end/src/App.jsx
+++ App/front-end/src/App.jsx
@@ -1,20 +1,38 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "./components/home/Home";
-import Register from "./components/register/Register"
-import Header from "./components/header/Header";
-import Login from "./components/login/Login";
-import Logout from "./components/logout/Logout";
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/home/Home';
+import Register from './components/register/Register';
+import Header from './components/header/Header';
+import Login from './components/login/Login';
+import Logout from './components/logout/Logout';
+import PrivateRoute from './components/util/privateRoute';
+import { useAuth } from './context/AuthContext';
 
 const App = () => {
+  const { user } = useAuth();
+
   return (
     <>
       <Header />
+
       <div className="outer-box">
         <Routes>
+          <Route
+            path="/register"
+            element={
+              <PrivateRoute isAllowed={!user}>
+                <Register />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <PrivateRoute isAllowed={!user}>
+                <Login />
+              </PrivateRoute>
+            }
+          />
           <Route path="/" element={<Home />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="logout" element={<Logout />} />
         </Routes>
       </div>
     </>
